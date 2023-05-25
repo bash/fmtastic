c4be7506da8e869dbaffe54f40438fdb1df0e9e1
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-05-25 21:20:38 +0200

    Update readme and module docs
    Resolves #7
    Resolves #6

--- readme.md
+++ readme.md
@@ -1,5 +1,30 @@
 # fmtastic ✨
-A **fantastic** crate for **fmt**ing numbers using the appropriate unciode characters via the `Display` trait. ✨ \
+A **fantastic** crate for **fmt**ing numbers using the appropriate unciode characters via the [`Display`] trait. ✨ \
 Supports vulgar fractions, super- and subscript.
 
 Contributions are welcome for more formats.
+
+## Features
+
+### [Vulgar Fractions]
+Creates beautiful unicode fractions like ¼ or ¹⁰⁄₃.
+```rust
+use fmtastic::VulgarFraction;
+
+assert_eq!("¹⁰⁄₃", format!("{}", VulgarFraction::new(10, 3)));
+assert_eq!("¼", format!("{}", VulgarFraction::new(1, 4)));
+```
+
+### Sub- and superscript
+Formats integers as sub- or superscript. 
+
+```rust
+use fmtastic::{Subscript, Superscript};
+
+assert_eq!("x₁", format!("x{}", Subscript(1)));
+assert_eq!("n²", format!("n{}", Superscript(2)));
+```
+
+
+[Vulgar Fractions]: https://en.wikipedia.org/wiki/Fraction_(mathematics)#Simple,_common,_or_vulgar_fractions
+[`Display`]: https://doc.rust-lang.org/std/fmt/trait.Display.html
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,29 @@
-//! A **fantastic** crate for **fmt**ing numbers using the appropriate unciode characters via the [`Display`][`std::fmt::Display`] trait. ✨ \
+//! A **fantastic** crate for **fmt**ing numbers using the appropriate unciode characters via the [`Display`](std::fmt::Display) trait. ✨ \
 //! Supports vulgar fractions, super- and subscript.
 //!
-//! Contributions are welcome for more formats.
+//! # [Vulgar Fractions]
+//! Creates beautiful unicode fractions like ¼ or ¹⁰⁄₃.
+//! ```rust
+//! # use fmtastic::VulgarFraction;
+//! assert_eq!("¹⁰⁄₃", format!("{}", VulgarFraction::new(10, 3)));
+//! assert_eq!("¼", format!("{}", VulgarFraction::new(1, 4)));
+//! ```
+//!
+//! # Sub- and superscript
+//! Formats integers as sub- or superscript.
+//!
+//! ```rust
+//! # use fmtastic::{Subscript, Superscript};
+//! assert_eq!("x₁", format!("x{}", Subscript(1)));
+//! assert_eq!("n²", format!("n{}", Superscript(2)));
+//! ```
+//!
+//! [Vulgar Fractions]: https://en.wikipedia.org/wiki/Fraction_(mathematics)#Simple,_common,_or_vulgar_fractions
 
 mod sub_superscript;
 pub use sub_superscript::*;
 mod fraction;
 pub use fraction::*;
+
+#[doc = include_str!("../readme.md")]
+mod doctest_readme {}
